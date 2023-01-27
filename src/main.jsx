c6291fa40976b95c70f7f5571a98e3991d50b406
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-01-27 17:26:05 -0500

    Primer Commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Componentes
+import GiftExpertApp from "./GiftExpertApp";
+
+// Estilos
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GiftExpertApp />
+  </React.StrictMode>
+);
